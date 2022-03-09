be4d3c252052af0642ead28d6ef9846f03abc7af
Robin Devitt <robin@humanmade.com>
2022-03-09 09:14:35 +0200

    Documentation QA Process.

---
+++ tests/acceptance/DocumentationCest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Tests for Altis Documentation module.
+ *
+ * phpcs:disable WordPress.Files, WordPress.NamingConventions, PSR1.Classes.ClassDeclaration.MissingNamespace, HM.Functions.NamespacedFunctions
+ */
+
+/**
+ * Test core module admin features.
+ */
+class DocumentationCest {
+
+	/**
+	 * Documentation module renders correctly.
+	 *
+	 * @param AcceptanceTester $I Tester
+	 *
+	 * @return void
+	 */
+	public function testDocumentationRender( AcceptanceTester $I ) {
+
+		$I->wantToTest( 'Documentation module renders correctly.' );
+		$I->loginAsAdmin();
+
+		$I->amOnAdminPage( 'admin.php?page=altis-documentation' );
+		// Documentation title
+		$I->seeElement( '.altis-ui__doc-title' );
+
+	}
+
+}
