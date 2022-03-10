f8464a33c86c5e5e1dad88a43a77726f93cf014b
Robin Devitt <robin@humanmade.com>
2022-03-10 10:38:16 +0200

    Check the documentation link is there and navigate to a different modules documentation.

--- tests/acceptance/DocumentationCest.php
+++ tests/acceptance/DocumentationCest.php
@@ -11,6 +11,21 @@
 class DocumentationCest {
 
 	/**
+	 * Documantation link is shown.
+	 *
+	 * @param AcceptanceTester $I Tester
+	 *
+	 */
+	public function testDocumentationLink( AcceptanceTester $I ) {
+		$I->wantToTest( 'Documantation link is shown.' );
+		$I->loginAsAdmin();
+		$I->amOnAdminPage( '/' );
+
+		$I->moveMouseOver( '.altis-logo-wrapper' );
+		$I->seeLink( 'Documentation' );
+	}
+
+	/**
 	 * Documentation module renders correctly.
 	 *
 	 * @param AcceptanceTester $I Tester
@@ -25,5 +40,27 @@ class DocumentationCest {
 		// Documentation title.
 		$I->seeElement( '.altis-ui__doc-title' );
 	}
+
+	/**
+	 * Navigate to CMS module and confirm there is content.
+	 *
+	 * @param AcceptanceTester $I Tester
+	 *
+	 * @return void
+	 */
+	public function testDocumentationNavigation( AcceptanceTester $I ) {
+		$I->wantToTest( 'Navigate to CMS module and confirm there is content.' );
+		$I->loginAsAdmin();
+		$I->amOnAdminPage( 'admin.php?page=altis-documentation' );
+
+		// CMS Module.
+		$I->see( 'CMS', 'li' );
+		$I->click( 'CMS' );
+
+		// See the CMS H! title.
+		$I->see( 'CMS', 'h1' );
+	}
+
+
 
 }
