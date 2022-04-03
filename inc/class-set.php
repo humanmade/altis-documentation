3b1251385db66f16b7117972aa22d6ae2a7746e5
Mike Little <mike@zed1.com>
2022-04-03 15:32:32 +0100

    Add Set id and default group id to class.

--- inc/class-set.php
+++ inc/class-set.php
@@ -15,6 +15,13 @@ namespace Altis\Documentation;
  */
 class Set {
 	/**
+	 * Set id . E.g. 'dev-docs', 'user-documentation', etc.
+	 *
+	 * @var string
+	 */
+	protected $id = '';
+
+	/**
 	 * Set title . E.g. 'Developer documentation', 'User documentation', etc.
 	 *
 	 * @var string
@@ -29,11 +36,20 @@ class Set {
 	protected $groups = [];
 
 	/**
+	 * Default group ID.
+	 *
+	 * @var string
+	 */
+	protected $default_group_id = '';
+
+	/**
 	 * Constructor.
 	 *
+	 * @param string $id Set id.
 	 * @param string $title Set title.
 	 */
-	public function __construct( string $title = '' ) {
+	public function __construct( string $id = '', string $title = '' ) {
+		$this->id = $id;
 		$this->title = $title;
 	}
 
@@ -47,10 +63,19 @@ class Set {
 	}
 
 	/**
+	 * Get the Set id.
+	 *
+	 * @return string
+	 */
+	public function get_id() : string {
+		return $this->id;
+	}
+
+	/**
 	 * Add Group to the documentation set.
 	 *
-	 * @param string $id    Group Id
-	 * @param Group  $group Group object.
+	 * @param string $id Group Id
+	 * @param Group $group Group object.
 	 */
 	public function add_group( string $id, Group $group ) : void {
 		$this->groups[ $id ] = $group;
@@ -63,7 +88,15 @@ class Set {
 	 */
 	public function get_groups() : array {
 
-		return $this->groups;
+		/**
+		 * Filter documentation groups for this set.
+		 *
+		 * This allows modules to register additional documentation groups in this set.
+		 *
+		 * @param Group[] $docs Map of group ID to Group object.
+		 * @param string $set_id The documentation set id.
+		 */
+		return apply_filters( 'altis.documentation.groups', $this->groups, $this->id );
 	}
 
 	/**
@@ -76,6 +109,33 @@ class Set {
 	public function get_group( string $id ) : ?Group {
 
 		return $this->groups[ $id ] ?? null;
+	}
+
+	/**
+	 * Get the default group ID.
+	 *
+	 * @return string.
+	 */
+	public function get_default_group_id() : string {
+		if ( ! empty( $this->default_group_id ) ) {
+			return $this->default_group_id;
+		}
+
+		if ( count( $this->groups ) ) {
+
+			return array_keys( $this->groups )[0];
+		}
+
+		return '';
+	}
+
+	/**
+	 * Set the default group ID.
+	 *
+	 */
+	public function set_default_group_id( string $group_id ) : void {
+
+		$this->default_group_id = $group_id;
 	}
 
 }
